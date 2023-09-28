f0fb8a368cc0c8f93d8475902af2643f30a45b6a
Thiago <ticolls@hotmail.com>
2023-09-28 13:50:41 -0300

    feat: add immediatly tick

--- handler/handler.go
+++ handler/handler.go
@@ -4,51 +4,58 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Ticolls/remail/client"
 	"github.com/Ticolls/remail/email"
+	"github.com/Ticolls/remail/models"
 	"github.com/Ticolls/remail/utils"
 )
 
-func HandleDailyEmail() {
+func Init(tasks *[]models.Task) {
+	for {
+		curentTime := time.Now()
 
-	ticker := time.NewTicker(1 * time.Hour)
+		if curentTime.Hour() == 13 && curentTime.Minute() == 45 {
+			fmt.Println("São 13 horas.")
+			EmailTaskHandler(tasks)
+		}
+	}
+}
+
+func EmailTaskHandler(tasks *[]models.Task) {
+
+	ticker := time.NewTicker(24 * time.Hour)
 	quit := make(chan struct{})
+
+	sendTodayTasks(tasks)
 
 	for {
 		select {
 		case <-ticker.C:
-			curentTime := time.Now()
-
-			if curentTime.Hour() == 8 {
-				err, tasks := client.GetTasks()
-
-				if err != nil {
-					panic(err)
-				}
-
-				todayTasks := utils.GetTodayTasks(tasks)
-
-				var message string
-
-				if len(todayTasks) == 0 {
-					message = "Sem tarefas para hoje!"
-				} else {
-					message = utils.BuildMessage(todayTasks)
-				}
-
-				fmt.Println(message)
-
-				err = email.SendEmail("Tarefas do dia", message)
-
-				if err != nil {
-					panic(err)
-				}
-			}
-
+			sendTodayTasks(tasks)
 		case <-quit:
 			ticker.Stop()
 			return
 		}
 	}
 
+}
+
+func sendTodayTasks(tasks *[]models.Task) {
+	todayTasks := utils.GetTodayTasks(tasks)
+
+	var message string
+
+	if len(todayTasks) == 0 {
+		message = "Sem tarefas para hoje!"
+	} else {
+		message = utils.BuildMessage(todayTasks)
+	}
+
+	err := email.SendEmail("Tarefas do dia", message)
+
+	if err != nil {
+		fmt.Printf("error: %s\n", err.Error())
+		return
+	}
+
+	fmt.Println("Email enviado com sucesso!")
 }
