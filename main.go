d9544ac076ada19971e2d696b27c61a9d55d2d86
Thiago <ticolls@hotmail.com>
2023-10-04 00:02:10 -0300

    feat: add error handling

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/Ticolls/remail/client"
 	"github.com/Ticolls/remail/email"
 	"github.com/Ticolls/remail/handler"
@@ -8,9 +11,16 @@ import (
 
 func main() {
 
-	email.Init()
+	err := email.Init()
+
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
 
 	client.Init()
+
+	fmt.Println("Everything ok with email and client services, waiting for ticks...")
 
 	handler.Init()
 
